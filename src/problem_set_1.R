a363e345503707f3e2ac8fffda857e52c56d4c11
Joshua Burkhart <burkhart.joshua@gmail.com>
2017-03-21 11:51:59 -0700

    now using least squares for Kd eqn fitting

--- src/problem_set_1.R
+++ src/problem_set_1.R
@@ -1,5 +1,6 @@
 # Joshua Burkhart
 # 3/14/2017
+# 3/21/2017 (revised)
 # PHPH 618
 # Problem Set 1
 
@@ -12,40 +13,35 @@ setwd("~/SoftwareProjects/PHPH618/src")
 binding_data <- read.csv("../data/problem_set_1.csv")
 
 # log plot
-plot(log2(binding_data$ligand_nM),binding_data$fraction_bound)
+plot(log10(binding_data$ligand_nM),binding_data$fraction_bound)
 
-# create model of the form Y = (1)x + (2)x^2 + (3)x^3 + (Intercept)
-nonlinear_model <- lm(binding_data$fraction_bound ~ poly(log2(binding_data$ligand_nM),3,raw = TRUE))
+L  <- binding_data$ligand_nM
+Kd.init.guess <- 1 # any number works...
+Fb  <- binding_data$fraction_bound
 
-# (1) = 0.0281644
-# (2) = 0.0170429
-# (3) = -0.0010036
-# (Intercept) = -0.0561749
-# Adjusted R-squared:  0.9719
-nonlinear_model %>% summary()
+# fitting equation
+nonlinear_model2 <- nls(Fb ~ (1 / (Kd/L + 1)), start = c(Kd = Kd.init.guess),model = TRUE)
+
+#Formula: Fb ~ (1/(Kd/L + 1))
+#Parameters:
+#   Estimate Std. Error t value Pr(>|t|)    
+#Kd    67.29       4.47   15.05 4.86e-10 ***
+nonlinear_model2 %>% summary()
 
 # plot nonlinear model
-lines(log2(binding_data$ligand_nM),predict(nonlinear_model))
+lines(log10(binding_data$ligand_nM),predict(nonlinear_model2))
 
 # half_max = 0.4925
 half_max <- max(binding_data$fraction_bound) / 2
 half_max_r <- round(half_max,digits = 3)
 
-# 0.4925 = 0.0281644x + 0.0170429x^2 + -0.0010036x^3 + -0.0561749
-library(polynom)
-pnom <- polynomial(nonlinear_model$coefficients)
-
-pnom
-
-# -5.583139  5.862778 16.701541
-solve(pnom,b=half_max)
-
-# unlog Kd
-Kd <- 5.862778
+# log10 Kd for plotting
+Kd <- nonlinear_model2 %>% summary() %>% coefficients() %>% .[1]
 Kd_r <- round(Kd,digits = 3)
+log10Kd_r <- round(log10(Kd),3)
 
 abline(h=half_max)
-abline(v=Kd)
+abline(v=log10Kd_r)
 
-text(x=Kd - 3,y=half_max + .1,labels = paste("(",Kd_r,",",half_max_r,")",sep=""))
-title(paste("Half-Max = ",half_max_r,", Kd = ",Kd_r,sep=""))
+text(x=log10(Kd) - .6,y=half_max + .06,labels = paste("(",log10Kd_r,",",half_max_r,")",sep=""))
+title(paste("Half-Max = ",half_max_r,", Kd = 10^",log10Kd_r," = ",Kd_r,sep=""))

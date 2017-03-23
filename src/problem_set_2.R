335d87ec81cf1b36e740b6418d4c59477c0b4c1e
Joshua Burkhart <burkhart.joshua@gmail.com>
2017-03-23 15:45:39 -0700

    now only using hill eqn

--- src/problem_set_2.R
+++ src/problem_set_2.R
@@ -22,15 +22,17 @@ binding_data$fraction_bound
 
 L  <- binding_data$ligand_uM
 Kd.init.guess <- 60
+n.init.guess <- 1
 Fb  <- binding_data$fraction_bound
 
 # fitting equation
-nonlinear_model2 <- nls(Fb ~ (1 / (Kd/L + 1)), start = c(Kd = Kd.init.guess),model = TRUE,trace=TRUE)
+nonlinear_model2 <- nls(Fb ~ (1 / ((Kd/L)^n + 1)), start = c(Kd = Kd.init.guess,n = n.init.guess),model = TRUE,trace=TRUE)
 
-#Formula: Fb ~ (1/(Kd/L + 1))
+#Formula: Fb ~ (1/((Kd/L)^n + 1))
 #Parameters:
-#   Estimate Std. Error t value Pr(>|t|)   
-#Kd    62.82      18.22   3.447  0.00546 **
+#   Estimate Std. Error t value Pr(>|t|)    
+#Kd  77.1000     1.7494   44.07 8.69e-13 ***
+#n    3.4398     0.2516   13.67 8.50e-08 ***
 nonlinear_model2 %>% summary()
 
 # plot nonlinear model
@@ -45,31 +47,14 @@ Kd <- nonlinear_model2 %>% summary() %>% coefficients() %>% .[1]
 Kd_r <- round(Kd,digits = 3)
 log10Kd_r <- round(log10(Kd),3)
 
+n <- nonlinear_model2 %>% summary() %>% coefficients() %>% .[2]
+
 abline(h=half_max)
 abline(v=log10Kd_r)
 
 text(x=log10(Kd) - .6,y=half_max + .06,labels = paste("(",log10Kd_r,",",half_max_r,")",sep=""))
 title(paste("Half-Max = ",half_max_r,", Kd = 10^",log10Kd_r," = ",Kd_r,sep=""))
 
-# Hill Equation (based on https://github.com/dritoshi/Fitting-Hill-equation/blob/master/bin/hill.r)
-L  <- binding_data$ligand_uM
-n.init.guess <- 1
-Fb  <- binding_data$fraction_bound
-
-# initial
-
-
-# fitting Hill equation
-nonlinear_hill_model <- nls(Fb ~ (L^n / (Kd + L^n)), start = c(n = n.init.guess))
-
-#Formula: Fb ~ (L^n/(Kd + L^n))
-#Parameters:
-#  Estimate Std. Error t value Pr(>|t|)    
-#n  1.03272    0.06766   15.26 9.48e-09 ***
-nonlinear_hill_model %>% summary()
-
-n <- nonlinear_hill_model %>% summary() %>% coefficients() %>% .[1]
-
-# K0.5^1.03272 = Kd
-# K0.5 = Kd^(1/0.52436) = 32.5805
+# Kd = K0.5^n = K0.5^3.4398 = 77.1000
+# K0.5 = Kd^(1/n) = 77.1000^(1/3.4398) = 3.536644
 Kd^(1/n)

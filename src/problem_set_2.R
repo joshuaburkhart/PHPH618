ddf74645495dd743f3f3c5e158ca0e20ef0e46e7
Joshua Burkhart <burkhart.joshua@gmail.com>
2017-03-23 17:45:11 -0700

    now using Ka for problem_set_2...

--- src/problem_set_2.R
+++ src/problem_set_2.R
@@ -21,17 +21,17 @@ binding_data$ligand_uM
 binding_data$fraction_bound
 
 L  <- binding_data$ligand_uM
-Kd.init.guess <- 60
+Ka.init.guess <- 60
 n.init.guess <- 1
 Fb  <- binding_data$fraction_bound
 
 # fitting equation
-nonlinear_model2 <- nls(Fb ~ (1 / ((Kd/L)^n + 1)), start = c(Kd = Kd.init.guess,n = n.init.guess),model = TRUE,trace=TRUE)
+nonlinear_model2 <- nls(Fb ~ (1 / ((Ka/L)^n + 1)), start = c(Ka = Ka.init.guess,n = n.init.guess),model = TRUE,trace=TRUE)
 
-#Formula: Fb ~ (1/((Kd/L)^n + 1))
+#Formula: Fb ~ (1/((Ka/L)^n + 1))
 #Parameters:
 #   Estimate Std. Error t value Pr(>|t|)    
-#Kd  77.1000     1.7494   44.07 8.69e-13 ***
+#Ka  77.1000     1.7494   44.07 8.69e-13 ***
 #n    3.4398     0.2516   13.67 8.50e-08 ***
 nonlinear_model2 %>% summary()
 
@@ -43,18 +43,14 @@ half_max <- max(binding_data$fraction_bound) / 2
 half_max_r <- round(half_max,digits = 3)
 
 # log10 Kd for plotting
-Kd <- nonlinear_model2 %>% summary() %>% coefficients() %>% .[1]
-Kd_r <- round(Kd,digits = 3)
-log10Kd_r <- round(log10(Kd),3)
+Ka <- nonlinear_model2 %>% summary() %>% coefficients() %>% .[1]
+Ka_r <- round(Ka,digits = 3)
+log10Ka_r <- round(log10(Ka),3)
 
 n <- nonlinear_model2 %>% summary() %>% coefficients() %>% .[2]
 
 abline(h=half_max)
-abline(v=log10Kd_r)
+abline(v=log10Ka_r)
 
-text(x=log10(Kd) - .6,y=half_max + .06,labels = paste("(",log10Kd_r,",",half_max_r,")",sep=""))
-title(paste("Half-Max = ",half_max_r,", Kd = 10^",log10Kd_r," = ",Kd_r,sep=""))
-
-# Kd = K0.5^n = K0.5^3.4398 = 77.1000
-# K0.5 = Kd^(1/n) = 77.1000^(1/3.4398) = 3.536644
-Kd^(1/n)
+text(x=log10(Ka) - .6,y=half_max + .06,labels = paste("(",log10Ka_r,",",half_max_r,")",sep=""))
+title(paste("Half-Max = ",half_max_r,", Ka = 10^",log10Ka_r," = ",Ka_r,sep=""))
